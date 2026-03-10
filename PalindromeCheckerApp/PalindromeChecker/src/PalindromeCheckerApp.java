326c4b21681c94e2e5d5c1fbc4c985c6eb425f43
UJWAL M <mujwal230@gmail.com>
2026-03-10 19:38:24 +0530

    UC13: Performance Comparison

--- PalindromeCheckerApp/PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,73 +1,65 @@
-// Palindrome strategy interface
-interface PalindromeStrategy {
-    boolean checkPalindrome(String text);
-}
-
-// Stack-based strategy
-class StackStrategy implements PalindromeStrategy {
-    @Override
-    public boolean checkPalindrome(String text) {
-        java.util.Stack<Character> stack = new java.util.Stack<>();
-        for (int i = 0; i < text.length(); i++) {
-            stack.push(text.charAt(i));
-        }
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) != stack.pop()) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-// Deque-based strategy
-class DequeStrategy implements PalindromeStrategy {
-    @Override
-    public boolean checkPalindrome(String text) {
-        java.util.Deque<Character> deque = new java.util.LinkedList<>();
-        for (int i = 0; i < text.length(); i++) {
-            deque.addLast(text.charAt(i));
-        }
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-// Context class to use strategy
-class PalindromeCheckerContext {
-    private PalindromeStrategy strategy;
-
-    public PalindromeCheckerContext(PalindromeStrategy strategy) {
-        this.strategy = strategy;
-    }
-
-    public boolean check(String text) {
-        return strategy.checkPalindrome(text);
-    }
-
-    public void setStrategy(PalindromeStrategy strategy) {
-        this.strategy = strategy;
-    }
-}
+import java.util.Stack;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
+
+    // Loop reverse method
+    public static boolean loopReverse(String text) {
+        String reversed = "";
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed += text.charAt(i);
+        }
+        return text.equals(reversed);
+    }
+
+    // Stack method
+    public static boolean stackMethod(String text) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : text.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : text.toCharArray()) {
+            if (c != stack.pop()) return false;
+        }
+        return true;
+    }
+
+    // Deque method
+    public static boolean dequeMethod(String text) {
+        Deque<Character> deque = new LinkedList<>();
+        for (char c : text.toCharArray()) {
+            deque.addLast(c);
+        }
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) return false;
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
 
         String word = "madam";
 
-        // Use Stack strategy
-        PalindromeCheckerContext context = new PalindromeCheckerContext(new StackStrategy());
-        System.out.println(word + " using StackStrategy: " +
-                (context.check(word) ? "Palindrome" : "Not Palindrome"));
+        // Loop Reverse timing
+        long start = System.nanoTime();
+        boolean result1 = loopReverse(word);
+        long end = System.nanoTime();
+        System.out.println("Loop Reverse: " + (result1 ? "Palindrome" : "Not Palindrome") +
+                " | Time: " + (end - start) + " ns");
 
-        // Switch to Deque strategy dynamically
-        context.setStrategy(new DequeStrategy());
-        System.out.println(word + " using DequeStrategy: " +
-                (context.check(word) ? "Palindrome" : "Not Palindrome"));
+        // Stack timing
+        start = System.nanoTime();
+        boolean result2 = stackMethod(word);
+        end = System.nanoTime();
+        System.out.println("Stack Method: " + (result2 ? "Palindrome" : "Not Palindrome") +
+                " | Time: " + (end - start) + " ns");
+
+        // Deque timing
+        start = System.nanoTime();
+        boolean result3 = dequeMethod(word);
+        end = System.nanoTime();
+        System.out.println("Deque Method: " + (result3 ? "Palindrome" : "Not Palindrome") +
+                " | Time: " + (end - start) + " ns");
     }
 }
